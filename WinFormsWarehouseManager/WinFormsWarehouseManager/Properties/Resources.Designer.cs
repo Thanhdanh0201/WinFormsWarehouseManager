0d1a2c921cb57a91412a4e4eaeb8e48874ee3ee0
danhdeptrai <danhnguyenhoctap12@gmail.com>
2025-12-04 02:30:20 +0700

    Add Custom Controls(Button, ComboBox, TextBox, DateTimePicker)

--- WinFormsWarehouseManager/WinFormsWarehouseManager/Properties/Resources.Designer.cs
+++ WinFormsWarehouseManager/WinFormsWarehouseManager/Properties/Resources.Designer.cs
@@ -83,6 +83,26 @@ namespace WinFormsWarehouseManager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap calendarDark {
+            get {
+                object obj = ResourceManager.GetObject("calendarDark", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap calendarWhite {
+            get {
+                object obj = ResourceManager.GetObject("calendarWhite", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap colorbutton {
             get {
                 object obj = ResourceManager.GetObject("colorbutton", resourceCulture);
